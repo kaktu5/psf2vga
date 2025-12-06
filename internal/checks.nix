702b27f8cb4a4248520b5b600f2d5b89f8b62012
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-12-06 16:06:51 +0100

    checks

---
+++ internal/checks.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  self,
+  system,
+}: let
+  inherit (lib.attrsets) attrValues;
+  inherit (pkgs) runCommandLocal;
+
+  packages' = self.packages.${system};
+in {
+  psf2vga = packages'.psf2vga.override {runTests = true;};
+
+  formatting =
+    runCommandLocal "psf2vga-formatting-check" {
+      nativeBuildInputs = attrValues {
+        inherit (pkgs) alejandra fd mdformat rustfmt taplo;
+      };
+    } ''
+      fd . ${self} -t f -e md -X mdformat --check --wrap 120 '{}'
+      fd . ${self} -t f -e nix -E Cargo.nix -X alejandra --check --quiet '{}'
+      fd . ${self} -t f -e rs -X rustfmt --check '{}'
+      RUST_LOG='warn' fd . ${self} -t f -e toml -X taplo check '{}'
+      touch $out
+    '';
+}
